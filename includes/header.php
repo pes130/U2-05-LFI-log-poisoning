13530478d029d97983f3fdb242d0786023e4b114
Pablo <pablo.esteban.sanchez@gmail.com>
2025-11-04 11:06:46 +0100

    Mejora del menú

--- includes/header.php
+++ includes/header.php
@@ -10,6 +10,21 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">CTF-LFI</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav"
+            aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="mainNav">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="/?page=about.php">Sobre mí</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/?page=help.php">Help</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
 <div class="container mt-4">
--- pages/help.php
+++ pages/help.php
@@ -1,3 +1,3 @@
 <h1>Help</h1>
-<p>Pruebas LFI: <code>?page=about.php</code> o intenta incluir rutas relativas como <code>?page=../../etc/passwd</code> (si quitas filtros).</p>
+<p>Ayuda? ayuda es lo que necesito yo!</p>
 
